db095607972f466f93d7592039dcbb017cae420e
Seva Lotoshnikov <vlotoshnikov@gmail.com>
2021-02-25 20:00:57 -0600

    Rename pip package to "ik.problem.function"

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ProblemFunctionSignature
 The library for parsing the coding problem function signature.
 
-String signature like `list[int32] f(a: list[list[str]])` becomes an instance of `Function`
-with a name, (composite) `Type` and a list of `Argument`s, each with a name and a `Type`.
+String signature like `list[int32] f(a: list[list[str]], b:char)`
+becomes an instance of `Signature`
+with name `f`, `Type` of value it returns, and a list of `Argument`s,
+each with its name and `Type`.
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="pfs",
+    name="ik.problem.function",
     version="0.1",
     description="The library for parsing the coding problem function signature.",
     long_description=long_description,
     url="https://github.com/InterviewKickstart/ProblemFunctionSignature",
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    packages=['ik.problem.function'],
+    package_dir={'ik.problem.function': 'src'},
 )
